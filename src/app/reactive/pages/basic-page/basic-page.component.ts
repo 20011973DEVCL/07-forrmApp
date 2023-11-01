6314b228e4b6a46e7cdaab33154c15daa4721ed7
Danilo Alarcon Lopez <danilo.alarcon.lopez@icloud.com>
2023-11-01 17:07:14 -0300

    formularios Basicos Ok

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -28,7 +28,32 @@ export class BasicPageComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-      this.myForm.reset(rtx5090);
+      // this.myForm.reset(rtx5090);
+  }
+
+  isValidField( field: string) {
+      return this.myForm.controls[field].errors
+        && this.myForm.controls[field].touched;
+  }
+
+  getFieldError( field: string) {
+
+    if (!this.myForm.controls[field])  return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+
+      switch(key) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Minimo ${ errors['minlength'].requiredLength } caracteres.`;
+      }
+    }
+
+    return "Hola Mundo";
   }
 
   onSave():void {
